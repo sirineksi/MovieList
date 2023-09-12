f0ef2edefc60a9d6c18ee6af19b6581e218a7754
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-09-12 14:12:17 +0300

    Initial Commit

---
+++ MovieList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieList
+//
+//  Created by ceksi on 12.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by ceksi on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
